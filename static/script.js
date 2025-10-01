ffab2d5c00636143f6408f3a51d482eeec2c186b
Ericreimann <e.reimann@edu.pucrs.br>
2025-09-30 21:38:45 -0300

    adicao do link api

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // === Config ===
-const API_BASE = ""; // mesma origem -> fetch("/predict/<modelo>")
+const API_BASE = "http://127.0.0.1:8000"; // mesma origem -> fetch("/predict/<modelo>")
 
 // === Estado do jogo ===
 let board = Array(9).fill("b"); // "x" | "o" | "b"
